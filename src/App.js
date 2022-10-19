bf22c9a02c4e28d9667fdce6c9330f3de743bf6e
Eric Aguerreberry <eric_aguerreberry@hotmail.com>
2022-10-18 21:01:20 -0300

    Desafío Menú e-commerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import Footer from "./components/Footer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import ItemListContainer from "./components/ItemListContainer";
+import NavBar from "./components/NavBar";
+import CartContextProvider from "./context/CartContext";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        {/* <CartContextProvider> */}
+          <NavBar />
+          <Routes>
+            {/* <Route path="/" element={ <ItemListContainer greeting="Bienvenido a la tienda online de Changas" />}/>
+            <Route path="/category/:category" element={<ItemListContainer />} />
+            <Route path="/product/:id" element={<ItemDetailContainer />} />
+            <Route path="/category/:category/product/:id" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} /> */}
+          </Routes>
+          <Footer />
+        {/* </CartContextProvider> */}
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    id: 1,
+    text: 'changas',
+    url: 'category/changas',
+  },
+  {
+    id: 2,
+    text: 'vans',
+    url: 'category/vans',
+  },
+  {
+    id: 3,
+    text: 'gorras',
+    url: 'category/gorra',
+  },
+]
+
